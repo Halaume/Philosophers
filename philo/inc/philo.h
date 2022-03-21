d7641a9b14d2a2d859637ac8af6bdb794f7f06ae
Guillaume HANQUER <ghanquer@e2r2p13.clusters.42paris.fr>
2022-03-21 15:45:41 +0100

    initial commit

---
+++ philo/inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghanquer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 15:00:51 by ghanquer          #+#    #+#             */
+/*   Updated: 2022/03/21 15:06:01 by ghanquer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <sting.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
